359d867d652e15b65a86e6ece885e8889bb8247a
Ritvik Kapila <ritvik.iitd@gmail.com>
2025-10-05 22:23:54 -0700

    m

--- assets/js/script.js
+++ assets/js/script.js
@@ -151,7 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
     scrollToTopBtn.style.cssText = `
         position: fixed;
         bottom: 20px;
-        right: 20px;
+        left: 20px;
         width: 50px;
         height: 50px;
         border-radius: 50%;
